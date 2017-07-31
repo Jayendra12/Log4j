4b99508d98c40ad20cb046d2a929b08c1ea636fb
Jayendra <kambli.jayendra@gmail.com>
2017-08-01 00:39:24 +0530

    Log4j

---
+++ src/com/seed/Log4jDemo.java
@@ -0,0 +1,13 @@
+package com.seed;
+
+import org.apache.log4j.Logger;
+
+public class Log4jDemo {
+
+	private static Logger log=Logger.getLogger(Log4jDemo.class);
+	public static void main(String[] args) {
+	  log.debug("first log4j Demo");
+
+	}
+
+}
